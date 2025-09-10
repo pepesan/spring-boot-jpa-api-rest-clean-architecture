492925c66ec7af9047b35cd15e93d3aebdb01d74
David Vaquero <pepesan@gmail.com>
2025-09-10 18:31:06 +0200

    corregido problema de dependencias de junit 5 con cambio en el pom.xml y modificada la clase e integración con junit

--- src/test/java/com/cursosdedesarrollo/springbootjpaapirestcleanarchitecture/bdd/RunCucumberTest.java
+++ src/test/java/com/cursosdedesarrollo/springbootjpaapirestcleanarchitecture/bdd/RunCucumberTest.java
@@ -1,17 +1,14 @@
 package com.cursosdedesarrollo.springbootjpaapirestcleanarchitecture.bdd;
 
 import io.cucumber.junit.platform.engine.Constants;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 /**
  * Descubre y ejecuta los .feature bajo classpath: features
  */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectPackages("features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,
         value = "com.cursosdedesarrollo.springbootjpaapirestcleanarchitecture.bdd.steps")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,
